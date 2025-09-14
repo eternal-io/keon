0fc18cc9aa03761fc099ef676b1f1c7cff95ec89
K--Aethiax <36335359+eternal-io@users.noreply.github.com>
2025-09-14 23:07:56 +0800

    feat: rewrite value::Value, add value::Float, remove value::Number.

--- src/value.rs
+++ src/value.rs
@@ -14,22 +14,28 @@ pub type ValueMap = BTreeMap<Value, Value>;
 pub type Record = BTreeMap<Str, Value>;
 
 #[derive(Debug, Clone, Copy)]
-pub enum Number {
-    Nat(u64),
-    Int(i64),
-    Float(f64),
-}
+pub struct Float(pub f64);
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Value {
-    /// Literal bool.
+    /// Literal Boolean value.
     Bool(bool),
 
-    /// Literal char.
+    /// Literal Unicode character.
     Char(char),
 
-    /// Literal number.
-    Number(Number),
+    /// Literal 64-bit floating-point number.
+    Float(Float),
+
+    /// Literal 64-bit unsigned integer.
+    Unsigned64(u64),
+    /// Literal 64-bit negative integer.
+    Negative64(u64),
+
+    /// Literal 128-bit unsigned integer.
+    Unsigned128(Box<u128>),
+    /// Literal 128-bit negative integer.
+    Negative128(Box<u128>),
 
     /// Literal string.
     String(Box<String>),
@@ -44,7 +50,7 @@ pub enum Value {
 
     /// Structural tuple, also used to represent 'unit'.
     ///
-    /// Guaranteed that the [`Values`] inside is non-empty, if it's provided by KEON.
+    /// Guaranteed that the [`Values`] inside is non-empty, if it's serialized/deserialized by KEON.
     Tuple(Option<Box<Values>>),
 
     /// Structural seq.
@@ -66,208 +72,76 @@ pub enum Nominal {
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Struct {
-    /// Aka 'unit'.
-    Tuple0,
-    /// Aka 'newtype', specialized due to [serde]'s design.
-    Tuple1(Value),
-    /// Just 'tuple', guaranteed that it has at least two values, if it's provided by KEON.
-    TupleN(Values),
-    /// Aka 'struct'.
+    Unit,
+    Tuple(Values),
     Record(Record),
 }
 
 //------------------------------------------------------------------------------
 
 macro_rules! impl_into {
-    ( $value:ident: $from:ty => $into:ident::$($tt:tt)* ) => {
-        impl From<$from> for $into {
+    ( $out:ty | $value:ident: $from:ty => $($tt:tt)* ) => {
+        impl From<$from> for $out {
             #[inline]
             #[allow(unused_variables)]
             fn from($value: $from) -> Self {
-                $into::$($tt)*
+                $($tt)*
             }
         }
     };
 }
 
-impl_into!( v: bool  => Value::Bool(v) );
-impl_into!( v: char  => Value::Char(v) );
-impl_into!( v: i8    => Value::Number(v.into()) );
-impl_into!( v: i16   => Value::Number(v.into()) );
-impl_into!( v: i32   => Value::Number(v.into()) );
-impl_into!( v: i64   => Value::Number(v.into()) );
-impl_into!( v: u8    => Value::Number(v.into()) );
-impl_into!( v: u16   => Value::Number(v.into()) );
-impl_into!( v: u32   => Value::Number(v.into()) );
-impl_into!( v: u64   => Value::Number(v.into()) );
-impl_into!( v: f32   => Value::Number(v.into()) );
-impl_into!( v: f64   => Value::Number(v.into()) );
-impl_into!( v: &str  => Value::String(Box::new(v.into())) );
-impl_into!( v: &[u8] => Value::ByteBuf(Box::new(v.into())) );
-impl_into!( v: ()    => Value::Tuple(None) );
-
-impl_into!( v: u8    => Number::Nat(v as _) );
-impl_into!( v: u16   => Number::Nat(v as _) );
-impl_into!( v: u32   => Number::Nat(v as _) );
-impl_into!( v: u64   => Number::Nat(v as _) );
-impl_into!( v: usize => Number::Nat(v as _) );
-impl_into!( v: i8    => Number::Int(v as _) );
-impl_into!( v: i16   => Number::Int(v as _) );
-impl_into!( v: i32   => Number::Int(v as _) );
-impl_into!( v: i64   => Number::Int(v as _) );
-impl_into!( v: isize => Number::Int(v as _) );
-impl_into!( v: f32   => Number::Float(v as _) );
-impl_into!( v: f64   => Number::Float(v as _) );
+impl_into!( Value | v: bool   => Value::Bool(v) );
+impl_into!( Value | v: char   => Value::Char(v) );
+impl_into!( Value | v: f32    => Value::Float(v.into()) );
+impl_into!( Value | v: f64    => Value::Float(v.into()) );
+impl_into!( Value | v: u8     => Value::Unsigned64(v as _) );
+impl_into!( Value | v: u16    => Value::Unsigned64(v as _) );
+impl_into!( Value | v: u32    => Value::Unsigned64(v as _) );
+impl_into!( Value | v: u64    => Value::Unsigned64(v as _) );
+impl_into!( Value | v: u128   => Value::Unsigned128(Box::new(v)) );
+impl_into!( Value | v: i8     => if v >= 0 { Value::Unsigned64 (         v as _ ) } else { Value::Negative64(          -v as _ ) } );
+impl_into!( Value | v: i16    => if v >= 0 { Value::Unsigned64 (         v as _ ) } else { Value::Negative64(          -v as _ ) } );
+impl_into!( Value | v: i32    => if v >= 0 { Value::Unsigned64 (         v as _ ) } else { Value::Negative64(          -v as _ ) } );
+impl_into!( Value | v: i64    => if v >= 0 { Value::Unsigned64 (         v as _ ) } else { Value::Negative64(          -v as _ ) } );
+impl_into!( Value | v: i128   => if v >= 0 { Value::Unsigned128(Box::new(v as _)) } else { Value::Negative128(Box::new(-v as _)) } );
+impl_into!( Value | v: &str   => Value::String(Box::new(v.into())) );
+impl_into!( Value | v: String => Value::String(Box::new(v.into())) );
+impl_into!( Value | v: &[u8]  => Value::ByteBuf(Box::new(v.into())) );
+impl_into!( Value | v: ()     => Value::Tuple(None) );
 
 //------------------------------------------------------------------------------
 
-// impl From<Option<Box<Value>>> for Container {
-//     fn from(value: Option<Box<Value>>) -> Self {
-//         Self::Maybe(value)
-//     }
-// }
+impl_into!( Float | v: f32 => Float(v as _) );
+impl_into!( Float | v: f64 => Float(v as _) );
 
-// impl<T: Into<Value>> From<Option<T>> for Container {
-//     fn from(value: Option<T>) -> Self {
-//         Self::Maybe(value.map(|v| Box::new(v.into())))
-//     }
-// }
+impl Eq for Float {}
 
-// impl<T> From<&[T]> for Container
-// where
-//     T: Into<Value> + Clone,
-// {
-//     fn from(value: &[T]) -> Self {
-//         Self::Seq(Box::new(Vec::from_iter(value.iter().cloned().map(Into::into))))
-//     }
-// }
-
-// impl<T, const N: usize> From<[T; N]> for Container
-// where
-//     T: Into<Value>,
-// {
-//     fn from(value: [T; N]) -> Self {
-//         Self::Seq(Box::new(Vec::from_iter(value.into_iter().map(Into::into))))
-//     }
-// }
-
-// impl<K, V> From<&[(K, V)]> for Container
-// where
-//     K: Into<Value> + Clone,
-//     V: Into<Value> + Clone,
-// {
-//     fn from(value: &[(K, V)]) -> Self {
-//         Self::Map(Box::new(BTreeMap::from_iter(
-//             value.iter().cloned().map(|(k, v)| (k.into(), v.into())),
-//         )))
-//     }
-// }
-
-// impl<K, V, const N: usize> From<[(K, V); N]> for Container
-// where
-//     K: Into<Value>,
-//     V: Into<Value>,
-// {
-//     fn from(value: [(K, V); N]) -> Self {
-//         Self::Map(Box::new(BTreeMap::from_iter(
-//             value.into_iter().map(|(k, v)| (k.into(), v.into())),
-//         )))
-//     }
-// }
-
-//------------------------------------------------------------------------------
-
-impl Number {
+impl PartialEq for Float {
     #[inline]
-    pub const fn to_i64_lossy(self) -> i64 {
-        match self {
-            Self::Nat(n) => match n >= i64::MAX as u64 {
-                true => i64::MAX,
-                false => n as i64,
-            },
-            Self::Int(i) => i,
-            Self::Float(f) => f.clamp(i64::MIN as f64, i64::MAX as f64) as i64,
-        }
-    }
-
-    #[inline]
-    pub const fn to_u64_lossy(self) -> u64 {
-        match self {
-            Self::Nat(n) => n,
-            Self::Int(i) => match i >= 0 {
-                true => i as u64,
-                false => 0,
-            },
-            Self::Float(f) => f.clamp(u64::MIN as f64, u64::MAX as f64) as u64,
-        }
-    }
-
-    #[inline]
-    pub const fn to_f64_lossy(self) -> f64 {
-        match self {
-            Self::Nat(n) => n as f64,
-            Self::Int(i) => i as f64,
-            Self::Float(f) => f,
-        }
-    }
-}
-
-impl PartialEq for Number {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Nat(a), Self::Nat(b)) => a == b,
-            (Self::Int(a), Self::Int(b)) => a == b,
-            (Self::Float(a), Self::Float(b)) => a.is_nan() && b.is_nan() || a == b,
-            _ => false,
-        }
+        let (x, y) = (self.0, other.0);
+        x.is_nan() && y.is_nan() || x == y
     }
 }
 
-impl Eq for Number {}
-
-/// In order to be able to use [`Number`] as a map key,
-/// `NaN` is greater than any other number and equal to themselves.
-#[allow(clippy::non_canonical_partial_ord_impl)]
-impl PartialOrd for Number {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(match self {
-            Number::Nat(n) => match other {
-                Number::Nat(m) => n.cmp(m),
-                Number::Int(_) => Ordering::Greater,
-                Number::Float(_) => Ordering::Less,
-            },
-            Number::Int(i) => match other {
-                Number::Nat(_) => Ordering::Less,
-                Number::Int(j) => i.cmp(j),
-                Number::Float(_) => Ordering::Less,
-            },
-            Number::Float(f) => match other {
-                Number::Nat(_) => Ordering::Greater,
-                Number::Int(_) => Ordering::Greater,
-                Number::Float(g) => match (f.is_nan(), g.is_nan()) {
-                    (false, false) => f.partial_cmp(g).unwrap(),
-                    (false, true) => Ordering::Less,
-                    (true, false) => Ordering::Greater,
-                    (true, true) => Ordering::Equal,
-                },
-            },
-        })
-    }
-}
-
-impl Ord for Number {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+impl Ord for Float {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(other).unwrap()
     }
 }
 
-impl Hash for Number {
+impl PartialOrd for Float {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.0.total_cmp(&other.0))
+    }
+}
+
+impl Hash for Float {
+    #[inline]
     fn hash<H: Hasher>(&self, state: &mut H) {
-        core::mem::discriminant(self).hash(state);
-        match self {
-            Number::Nat(n) => state.write_u64(*n),
-            Number::Int(i) => state.write_i64(*i),
-            Number::Float(f) => state.write_u64(f.to_bits()),
-        }
+        state.write_u64(self.0.to_bits());
     }
 }
